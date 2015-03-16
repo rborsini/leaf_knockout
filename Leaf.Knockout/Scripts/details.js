807b340b39ea05a10e506120a12a62fee337d528
rborsini <roberto.borsini@gmail.com>
2015-03-16 08:32:02 +0100

    Added details.js and Details.cshtml

---
+++ Leaf.Knockout/Scripts/details.js
@@ -0,0 +1,36 @@
+﻿var details = function () {
+
+    var viewModel = null;
+
+    return {
+        init: function (url, divId) {
+
+            ajaxRequest(url, divId);
+
+            setInterval(function () {
+                ajaxRequest(url, divId);
+            }, 3000);
+
+        }
+    };
+
+    function ajaxRequest(url, divId) {
+
+        $.ajax({
+            type: "GET",
+            url: url,
+            dataType: 'json',
+            success: function (result) {
+
+                if (viewModel == null) {
+                    viewModel = ko.mapping.fromJS(result);
+                    ko.applyBindings(viewModel, $(divId).get(0));
+                } else {
+                    ko.mapping.fromJS(result, viewModel);
+                }
+
+            }
+        });
+    }
+
+}();
