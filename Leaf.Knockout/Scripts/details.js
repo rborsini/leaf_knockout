14ac569fd9dadce6f4957bcddf4142cb641f3fc4
r.borsini <roberto.borsini@gmail.com>
2015-03-16 22:14:54 +0100

    Added highcharts

--- Leaf.Knockout/Scripts/details.js
+++ Leaf.Knockout/Scripts/details.js
@@ -22,12 +22,47 @@
             dataType: 'json',
             success: function (result) {
 
+                console.log("result", result);
+
                 if (viewModel == null) {
                     viewModel = ko.mapping.fromJS(result);
                     ko.applyBindings(viewModel, $(divId).get(0));
                 } else {
                     ko.mapping.fromJS(result, viewModel);
                 }
+
+                $('#chart').highcharts({
+                    title: {
+                        text: 'Monthly Values',
+                        x: -20 //center
+                    },
+                    xAxis: {
+                        categories: result.XValues
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Energy (kWh)'
+                        },
+                        plotLines: [{
+                            value: 0,
+                            width: 1,
+                            color: '#808080'
+                        }]
+                    },
+                    tooltip: {
+                        valueSuffix: 'kWh'
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle',
+                        borderWidth: 0
+                    },
+                    series: [{
+                        name: result.Name,
+                        data: result.YValues
+                    }]
+                });
 
             }
         });
