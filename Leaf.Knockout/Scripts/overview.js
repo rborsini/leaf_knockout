e4ce80916c80cc771bf624a5fff6158c26b0bdcc
r.borsini <roberto.borsini@gmail.com>
2015-03-20 14:32:30 +0100

    Implemented type filter

--- Leaf.Knockout/Scripts/overview.js
+++ Leaf.Knockout/Scripts/overview.js
@@ -2,14 +2,18 @@
 
     var viewModel = null;
 
+    // View type toggle (grid vs list)
     $('#wiew-toggle').on('click', function () {
-
         viewModel.viewType(viewModel.viewType() == 'list-item' ? 'grid-item' : 'list-item');
     });
 
     return {
         init: function (url, divId) {
+
+            // get list length from query string
             url = url + window.location.search;
+
+            // http request
             ajaxRequest(url, divId);
         }
     };
@@ -28,24 +32,40 @@
                     js.plants[i].link = '/Monitor/Details?id=' + js.plants[i].Id;
                 }
 
-                if (viewModel == null) {
-                    viewModel = ko.mapping.fromJS(js);
-                    viewModel.viewType = ko.observable('list-item');
-                    viewModel.searchField = ko.observable('');
+                // create model from json object
+                viewModel = ko.mapping.fromJS(js);
 
-                    viewModel.filteredElements = ko.computed(function () {
-                        return ko.utils.arrayFilter(viewModel.plants(), function (rec) {
-                            return (
-                                      (viewModel.searchField().length == 0 || rec.Name().toLowerCase().indexOf(viewModel.searchField().toLowerCase()) > -1)
-                                   )
-                        });
+                // init viewType to list mode
+                viewModel.viewType = ko.observable('list-item');
+
+                // init searchField in viewModel
+                viewModel.searchField = ko.observable('');
+                viewModel.pvType = ko.observable(false);
+                viewModel.hydroType = ko.observable(false);
+
+                viewModel.filteredElements = ko.computed(function () {
+                    return ko.utils.arrayFilter(viewModel.plants(), function (rec) {
+
+                        var searchByName = viewModel.searchField().length == 0 || rec.Name().toLowerCase().indexOf(viewModel.searchField().toLowerCase()) > -1;
+
+                        var pv = viewModel.pvType();
+                        var hydro = viewModel.hydroType();
+
+                        var searchByType = (!pv && !hydro) || (pv && rec.Type() == 'Photovoltaic') || (hydro && rec.Type() == 'Hydroelectric');
+
+                        return searchByName && searchByType;
                     });
+                });
 
+                viewModel.pvClick = function () {
+                    viewModel.pvType(!$('#photovoltaic_type').hasClass('active'));
+                };
 
-                    ko.applyBindings(viewModel, $(divId).get(0));
-                } else {
-                    ko.mapping.fromJS(js, viewModel);
-                }
+                viewModel.hydroClick = function () {
+                    viewModel.hydroType(!$('#hydroelectric_type').hasClass('active'));
+                };
+
+                ko.applyBindings(viewModel, $(divId).get(0));
 
             }
         });
